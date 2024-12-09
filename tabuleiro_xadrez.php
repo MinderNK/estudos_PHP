047d43fdd1ff1d147116f9c391f4ab5ca0348430
MinderNK <111465649+MinderNK@users.noreply.github.com>
2024-12-09 16:12:20 -0300

    Add files via upload

---
+++ tabuleiro_xadrez.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>xadrez</title>
+</head>
+<body>
+    <?php
+        $tabuleiro[8]['A'] ="torre preta";
+        $tabuleiro[8]['B']="bispo pretp";
+        $tabuleiro[8]['C'] ="cavalo preto";
+        $tabuleiro[8]['D']= "rainha preta";
+        $tabuleiro[8]['E']="rei preto";
+        $tabuleiro[8]['F']="bispo preto";
+        $tabuleiro[8]['G']="cavalo preto";
+        $tabuleiro[8]['H']="torre preta";
+
+        $tabuleiro[7]['A']= "Peao preto";
+        $tabuleiro[7]['B']= "Peao preto";
+        $tabuleiro[7]['C']= "Peao preto";
+        $tabuleiro[7]['D']= "Peao preto";
+        $tabuleiro[7]['E']= "Peao preto";
+        $tabuleiro[7]['F']= "Peao preto";
+        $tabuleiro[7]['G']= "Peao preto";
+        $tabuleiro[7]['H']= "Peao preto";
+
+
+        $tabuleiro[2]['A']= "Peao branco";
+        $tabuleiro[2]['B']= "Peao branco";
+        $tabuleiro[2]['C']= "Peao branco";
+        $tabuleiro[2]['D']= "Peao branco";
+        $tabuleiro[2]['E']= "Peao branco";
+        $tabuleiro[2]['F']= "Peao branco";
+        $tabuleiro[2]['G']= "Peao branco";
+        $tabuleiro[2]['H']= "Peao branco";
+        
+
+        $tabuleiro[1]['A']= "torre branca";
+        $tabuleiro[1]['B']= "bispo branco";
+        $tabuleiro[1]['C']= "cavalo branco";
+        $tabuleiro[1]['D']= "rainha branca";
+        $tabuleiro[1]['E']= "rei branco";
+        $tabuleiro[1]['F']= "bispo branco";
+        $tabuleiro[1]['G']= "cavalo branco";
+        $tabuleiro[1]['H']= "torre branca";
+        
+        print_r(value: $tabuleiro);
+        print_r(value: $tabuleiro);
+    ?>
+</body>
+</html>
